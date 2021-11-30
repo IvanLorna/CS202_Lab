62c9d2d044873c1a2a54c00544eab044db46c4f8
liuchenxi <liuchenxi@ainirobot.com>
2021-11-30 13:55:19 -0800

    First trial to implement clone. Get a panic after testing.

--- kernel/proc.c
+++ kernel/proc.c
@@ -144,6 +144,54 @@ found:
   return p;
 }
 
+void
+forkret2(void);
+
+struct proc*
+allocproc_thread(void *stack, int size)
+{
+  struct proc *p;
+
+  for(p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if(p->state == UNUSED) {
+      goto found;
+    } else {
+      release(&p->lock);
+    }
+  }
+  return 0;
+
+found:
+  p->pid = allocpid();
+  p->state = USED;
+
+  // Allocate a trapframe page.
+  if((p->trapframe = (struct trapframe *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  struct proc *fp = myproc();
+
+  // An empty user page table.
+  p->pagetable = fp->pagetable;
+  if(p->pagetable == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  // Set up new context to start executing at forkret,
+  // which returns to user space.
+  memset(&p->context, 0, sizeof(p->context));
+  p->context.ra = (uint64)forkret;
+  p->context.sp = p->kstack + PGSIZE;
+
+  return p;
+}
+
 // free a proc structure and the data hanging from it,
 // including user pages.
 // p->lock must be held.
@@ -265,6 +313,64 @@ growproc(int n)
   }
   p->sz = sz;
   return 0;
+}
+
+int
+clone(void *stack, int size)
+{
+  int i, pid;
+  struct proc *np;
+  struct proc *p = myproc();
+
+  printf("stack: %p\nsize: %d\n", stack, size);
+
+  // Allocate process.
+  // if((np = allocproc_thread(stack, size)) == 0){
+  //   return -1;
+  // }
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  np->pagetable = p->pagetable;
+  // Copy user memory from parent to child.
+  // if(uvmcopy(p->pagetable, np->pagetable, p->sz) < 0){
+  //   freeproc(np);
+  //   release(&np->lock);
+  //   return -1;
+  // }
+  np->sz = p->sz;
+
+  // copy saved user registers.
+  *(np->trapframe) = *(p->trapframe);
+
+  // Cause fork to return 0 in the child.
+  np->trapframe->a0 = 0;
+  np->trapframe->sp = (uint64)stack;
+
+  // increment reference counts on open file descriptors.
+  for(i = 0; i < NOFILE; i++)
+    if(p->ofile[i])
+      np->ofile[i] = filedup(p->ofile[i]);
+  np->cwd = idup(p->cwd);
+
+  safestrcpy(np->name, p->name, sizeof(p->name));
+
+  pid = np->pid;
+
+  release(&np->lock);
+
+  acquire(&wait_lock);
+  np->parent = p;
+  release(&wait_lock);
+
+  acquire(&np->lock);
+  np->state = RUNNABLE;
+  release(&np->lock);
+
+  printf("clone finish\n");
+
+  return pid;
 }
 
 // Create a new process, copying the parent.
@@ -502,6 +608,27 @@ yield(void)
   release(&p->lock);
 }
 
+void
+forkret2(void)
+{
+  // static int first = 1;
+
+  // // Still holding p->lock from scheduler.
+  release(&myproc()->lock);
+
+  // if (first) {
+  //   // File system initialization must be run in the context of a
+  //   // regular process (e.g., because it calls sleep), and thus cannot
+  //   // be run from main().
+  //   first = 0;
+  //   fsinit(ROOTDEV);
+  // }
+
+  printf("forkret2\n");
+
+  usertrapret();
+}
+
 // A fork child's very first scheduling by scheduler()
 // will swtch to forkret.
 void
@@ -653,11 +780,5 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
-}
-
-int clone(void * stack, int size)
-{
-	printf("stack: %p\nsize: %d\n", stack, size);
-	return 0;
 }
 
--- user/test.c
+++ user/test.c
@@ -2,12 +2,44 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define PGSIZE 4096
+
 int main(int argc, char *argv[])
 {
-	int n =0;
-	if (argc >= 2) n = atoi(argv[1]);
+	// int n =0;
+	// if (argc >= 2) n = atoi(argv[1]);
 
-	clone(&n, n);
+	void* freeptr = malloc(PGSIZE*4);
+	void* stack;
+	if(freeptr == 0)
+		return -1;
+	// if((uint64)freeptr % PGSIZE == 0)
+	// 	stack = freeptr;
+	// else
+	// 	stack = freeptr + (PGSIZE - ((uint64)freeptr % PGSIZE));
+
+	stack = (void *)(PGSIZE * (((uint64)freeptr + 2*PGSIZE) / PGSIZE));
+
+	// void *stack = (void *)malloc(1024);
+	printf("test stack = 0x%x\n", stack);
+	int ret = clone(stack, PGSIZE);
+	printf("test ret = %d\n");
+	if(ret == 0) {
+		printf("test 1\n");
+		int i = 0;
+		while(i < 1000) {
+			i++;
+			sleep(5);
+			printf("test 1 sleep i1 = %d\n", i);
+		}
+	} else {
+		int i = 0;
+		while(i < 1000) {
+			i++;
+			sleep(7);
+			printf("test 2 sleep i2 = %d\n", i);
+		}
+	}
 	exit(0);
 
 
